db372e1921735eb7ebd14b3faa1ebf192e2e57bd
tjclutter <tjclutter@outlook.com>
2023-09-21 11:21:21 -0400

    Final commit with comments

--- CT_Module2_Code.cpp
+++ CT_Module2_Code.cpp
@@ -1,26 +1,30 @@
-//============================================================================
-// Name        : CT_Module2_Code.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
+//Included libraries and
+//using namespace std
 #include <iostream>
 #include <string.h>
 using namespace std;
 
+//declare main function
 int main() {
+	//for loop to iterate three times
 	for(int i = 0; i < 3; i++){
+		//declare character array of size 100
 		char string1[100];
+		//prompt user for input
 		cout << "Enter string 1: ";
+		//get user input and store in string1
 		cin.getline(string1, 99);
 
+		//declare string array of size 100
 		char string2[100];
+		//prompt user for input
 		cout << "Enter string 2: ";
+		//store user input in string2
 		cin.getline(string2, 99);
 
+		//concatenate two strings
 		strcat(string1, string2);
+		//display concatenated string to user
 		cout << string1 << endl;
 	}
 }
